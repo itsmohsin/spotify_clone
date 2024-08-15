553616dd837997785924e43225a0c6dfa8e31555
mohsinsmannat <mohsinsmannat@gmail.com>
2024-08-15 08:36:46 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/core/configs/theme/app_theme.dart';
+import 'package:myapp/presentation/splash/pages/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spotify Clone',
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
+    );
+  }
+}
